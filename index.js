51051e60968737b24fa2bc1459fac17d579c8378
Dominik Piekarski <piekarski_d@o2.pl>
2022-05-06 18:51:09 +0200

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1. Please write a function that shows the usage of closures
+
+// 2. Please write a function that returns a sum of array items
+// example input [9, 1, 22, 0, 2]
+// example output 34
+
+// 3. Please write a recursive function that flattens a list of items
+// example input [[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5]]
+// example output [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5]
+
+// 4. Please write a function that finds all common elements of two arrays(only primitive types as array elements, order doesn't matter)
+// example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
+// example output ['b', 4, 76]
+
+// 5. Please write a function that finds all different elements of two arrays(only primitive types as array elements, order doesn't matter)
+// example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
+// example output ['a', 3, 21, 'c', 'e']
+
+// 6. Please write a function that takes two arrays of items and returns an array of tuples made from two input arrays at the same indexes. Excessive items should be dropped.
+// example input [1,2,3], [4,5,6,7]
+// example output [[1,4], [2,5], [3,6]]
+
+// 7. Please write a function which takes a path(path is an array of keys) and object, then returns value at this path. If value at path doesn't exists, return undefined.
+// example inputs ['a', 'b', 'c', 'd'], { a: { b: { c: { d: '23' } } } }
+// example output '23'
+
+// 8. Please write compare function which compares 2 objects for equality.
+// example input { a: 'b', c: 'd' }, { c: 'd', a: 'b' }  /// output true
+// example input { a: 'c', c: 'a' }, { c: 'd', a: 'b', q: 's' }  /// output false
+
+// 9. Please write a function which takes a list of keys and an object, then returns this object, just without keys from the list
+// example input ['color', 'size'], { color: 'Blue', id: '22', size: 'xl' }
+// example output { id: '22' }
+
